8f92f2964e23e2b2a341f8e59a4b684fe809d5bb
Joel Pena Navarro <joel_p13@hotmail.com>
2023-04-11 20:35:19 +0200

    useEffect and API working

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,8 +1,23 @@
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
+const apiURL = 'https://api.jsonbin.io/v3/b/6422b9c8c0e7653a0597d126';
+
+async function fetchData(api){
+    const data = await fetch(api);
+    const response = await data.json();
+
+    console.log("Response: ", response.record.length);
+}
 
 const HomePage = ()=>{
     //this shows the HomePage with all its components
+    useEffect( ()=>{
+        fetchData(apiURL);
+
+    }, [])
+
+
     return(
         <div>
             <h1>Doggy Daycare</h1>
